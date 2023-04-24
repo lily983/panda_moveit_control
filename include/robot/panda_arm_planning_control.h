489785100f397d664b79477497fd164380c5b0ca
WANG XIAOLI <71541515+lily983@users.noreply.github.com>
2023-04-24 18:02:38 +0800

    use clang format to organize header file and source file

--- include/robot/panda_arm_planning_control.h
+++ include/robot/panda_arm_planning_control.h
@@ -2,38 +2,44 @@
 
 #include <ros/ros.h>
 
-#include <moveit/planning_scene_interface/planning_scene_interface.h>
 #include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
 
 #include <math.h>
 
-class PandaArmPlanningControl
-{
-    public:
-    PandaArmPlanningControl(const double max_vel_scale = 0.01, const double max_acc_scale = 0.01);
-    ~PandaArmPlanningControl();
+class PandaArmPlanningControl {
+ public:
+  PandaArmPlanningControl(const double max_vel_scale = 0.01,
+                          const double max_acc_scale = 0.01);
+  ~PandaArmPlanningControl();
 
-    bool GoHome();
+  bool GoHome();
 
-    bool MoveToJointTarget(const std::vector<double> joint_target);
+  bool MoveToJointTarget(const std::vector<double> joint_target);
 
-    bool MoveToPoseTarget(const geometry_msgs::Pose pose_target);
+  bool MoveToPoseTarget(const geometry_msgs::Pose pose_target);
 
-    moveit_msgs::RobotTrajectory PlanningToJointTarget(const std::vector<double> joint_target);
+  moveit_msgs::RobotTrajectory PlanningToJointTarget(
+      const std::vector<double> joint_target);
 
-    moveit_msgs::RobotTrajectory PlanningToJointTarget(const std::vector<double> joint_target, const std::vector<double> start_joint_values);
+  moveit_msgs::RobotTrajectory PlanningToJointTarget(
+      const std::vector<double> joint_target,
+      const std::vector<double> start_joint_values);
 
-    moveit_msgs::RobotTrajectory PlanningToPoseTarget(const geometry_msgs::Pose pose_target);
+  moveit_msgs::RobotTrajectory PlanningToPoseTarget(
+      const geometry_msgs::Pose pose_target);
 
-    moveit_msgs::RobotTrajectory PlanningToPoseTarget(const geometry_msgs::Pose pose_target, const std::vector<double> start_joint_values);
+  moveit_msgs::RobotTrajectory PlanningToPoseTarget(
+      const geometry_msgs::Pose pose_target,
+      const std::vector<double> start_joint_values);
 
-    bool ExecuteTrajectory(const moveit_msgs::RobotTrajectory traj);
+  bool ExecuteTrajectory(const moveit_msgs::RobotTrajectory traj);
 
-    moveit::core::RobotState GetMoveGroupState();
+  moveit::core::RobotState GetMoveGroupState();
 
-    private:
-    std::string planning_group_;
-    moveit::planning_interface::MoveGroupInterface move_group_;
-    const moveit::core::JointModelGroup* joint_model_group_;
-    const std::vector<double> home_config_;
+ private:
+  std::string planning_group_;
+  moveit::planning_interface::MoveGroupInterface move_group_;
+  const moveit::core::JointModelGroup* joint_model_group_;
+  const std::vector<double> home_config_;
 };
--- include/stomp/opening_rotating.h
+++ include/stomp/opening_rotating.h
@@ -1,17 +1,18 @@
 #include "include/stomp/execute_stomp_traj.h"
 
-class OpeningRotating: ExecuteStompTraj
-{
-    public:
-    OpeningRotating(ros::NodeHandle n);
-    ~OpeningRotating();
-    bool CallbackExecuteStompTraj(panda_moveit_control::ExecuteStompTraj::Request &req,
-                                panda_moveit_control::ExecuteStompTraj::Response &res);
-    
-    void GenerateGraspPose(const geometry_msgs::TransformStamped tf_world_to_marker);
+class OpeningRotating : ExecuteStompTraj {
+ public:
+  OpeningRotating(ros::NodeHandle n);
+  ~OpeningRotating();
+  bool CallbackExecuteStompTraj(
+      panda_moveit_control::ExecuteStompTraj::Request& req,
+      panda_moveit_control::ExecuteStompTraj::Response& res);
 
-    private:
-    geometry_msgs::Pose grasp_pose_;
-    geometry_msgs::Pose pre_grasp_pose_;
-    geometry_msgs::Pose post_grasp_pose_;
+  void GenerateGraspPose(
+      const geometry_msgs::TransformStamped tf_world_to_marker);
+
+ private:
+  geometry_msgs::Pose grasp_pose_;
+  geometry_msgs::Pose pre_grasp_pose_;
+  geometry_msgs::Pose post_grasp_pose_;
 };
