2da5ec9098c90d6faa96b9d18ff2bef2377252c4
WANG XIAOLI <71541515+lily983@users.noreply.github.com>
2023-10-13 18:02:27 +0800

    remove function of adding/removing collision mesh from class ExecuteStompTraj

--- include/stomp/execute_stomp_traj.h
+++ include/stomp/execute_stomp_traj.h
@@ -1,16 +1,19 @@
 #pragma once
 
-#include "include/stomp/data_parse.h"
+#include "include/utils/data_parse.h"
 
 #include <eigen_conversions/eigen_msg.h>
 #include <moveit_msgs/DisplayTrajectory.h>
 #include <eigen3/Eigen/Core>
 
-#include "panda_moveit_control/AddCollisionMesh.h"
-#include "panda_moveit_control/RemoveCollisionMesh.h"
-
 typedef Eigen::Transform<double, 3, Eigen::Affine> T;
 
+/**
+ * \brief Class for subscribe STOMP planned trajectories and publish target
+ * execution trajectory. All STOMP tasks(scooping, pouring, transporting,
+ * openning) will use this class.
+ *
+ */
 class ExecuteStompTraj {
   public:
     ExecuteStompTraj(ros::NodeHandle& n);
@@ -24,8 +27,6 @@ class ExecuteStompTraj {
     moveit_msgs::RobotTrajectory GetTargetStompTrajectory(int num);
 
   protected:
-    void StoreMeshToRepo();
-
     /**
      * \brief Receive and store moveit_msgs::DisplayTrajectory
      * published by stomp
@@ -35,13 +36,7 @@ class ExecuteStompTraj {
     void CallbackStompTraj(const moveit_msgs::DisplayTrajectoryPtr& msg);
 
     ros::NodeHandle n_;
-    std::string web_mesh_path_;
-    std::string repo_mesh_path_;
     std::string stomp_traj_topic_;
     moveit_msgs::DisplayTrajectory all_stomp_trajectory_;
     ros::Subscriber sub_trajectory_;
-
-  public:
-    ros::ServiceClient client_add_collision_mesh_;
-    ros::ServiceClient client_remove_collision_mesh_;
 };
--- src/stomp/execute_stomp_traj.cpp
+++ src/stomp/execute_stomp_traj.cpp
@@ -1,43 +1,17 @@
 #include "include/stomp/execute_stomp_traj.h"
 
 ExecuteStompTraj::ExecuteStompTraj(ros::NodeHandle& n) : n_(n) {
-    StoreMeshToRepo();
-
-    // ros::param::get("~stomp_traj_topic", stomp_traj_topic_);
-    stomp_traj_topic_ = "/server_stomp_planning/display_planned_path";
+    n_.getParam("stomp_trajectory_topic", stomp_traj_topic_);
+    std::cout << stomp_traj_topic_ << "\n";
     sub_trajectory_ = n_.subscribe(stomp_traj_topic_, 1,
                                    &ExecuteStompTraj::CallbackStompTraj, this);
     ROS_INFO("Subscribe to STOMP trojectory in topic named %s",
              stomp_traj_topic_.c_str());
 
-    client_add_collision_mesh_ =
-        n_.serviceClient<panda_moveit_control::AddCollisionMesh>(
-            "add_collision_mesh");
-
-    client_remove_collision_mesh_ =
-        n_.serviceClient<panda_moveit_control::RemoveCollisionMesh>(
-            "remove_collision_mesh");
-
     ROS_INFO("[ExecuteStompTraj]: finished initialization!");
 }
 
 ExecuteStompTraj::~ExecuteStompTraj() {}
-
-void ExecuteStompTraj::StoreMeshToRepo() {
-    ros::param::get("~web_mesh_path", web_mesh_path_);
-    ros::param::get("~repo_mesh_path", repo_mesh_path_);
-    std::cout << web_mesh_path_ << "\n";
-    std::cout << repo_mesh_path_ << "\n";
-
-    std::string web_scene_path, web_obj_path, repo_scene_path, repo_obj_path;
-    web_scene_path = web_mesh_path_ + "/mesh_scene.obj";
-    web_obj_path = web_mesh_path_ + "/mesh_obj.obj";
-    repo_scene_path = repo_mesh_path_ + "/mesh_scene.obj";
-    repo_obj_path = repo_mesh_path_ + "/mesh_obj.obj";
-
-    CopyMeshFromWebToRepo(web_scene_path, repo_scene_path);
-    CopyMeshFromWebToRepo(web_obj_path, repo_obj_path);
-}
 
 void ExecuteStompTraj::CallbackStompTraj(
     const moveit_msgs::DisplayTrajectoryPtr& msg) {
