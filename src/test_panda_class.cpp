60b8b03081eb6ff6dacfe0d0e17bb89c34f5d710
WANG XIAOLI <71541515+lily983@users.noreply.github.com>
2023-10-16 12:45:50 +0800

    change to meaningful name

--- src/test_panda_class.cpp
+++ src/test_panda_class.cpp
@@ -15,11 +15,11 @@ int main(int argc, char** argv) {
 
     ROS_INFO("==========Send arm to new config===========");
     ros::Duration(2.0).sleep();
-    std::vector<double> new_config = {
+    std::vector<double> test_class_config = {
         -0.005279347751289606, -0.2439806076267309, -0.015340340548884663,
         -2.286185151819128,    0.11115300154485346, 1.9794540780915153,
         0.8009687196584763};
-    panda_arm.MoveToJointTarget(new_config);
+    panda_arm.MoveToJointTarget(test_class_config);
 
     ROS_INFO("==========Test Gripper==========");
     GripperMoveAction(0.02);
@@ -31,15 +31,15 @@ int main(int argc, char** argv) {
     panda_arm.GoHome();
 
     ROS_INFO("==========Send arm to pose target============");
-    geometry_msgs::Pose target_pose;
-    target_pose.orientation.x = 0.939;
-    target_pose.orientation.y = 0.063;
-    target_pose.orientation.z = 0.3;
-    target_pose.orientation.w = -0.156;
-    target_pose.position.x = 0.3;
-    target_pose.position.y = -0.224;
-    target_pose.position.z = 0.501;
-    panda_arm.MoveToPoseTarget(target_pose);
+    geometry_msgs::Pose test_class_pose;
+    test_class_pose.orientation.x = 0.939;
+    test_class_pose.orientation.y = 0.063;
+    test_class_pose.orientation.z = 0.3;
+    test_class_pose.orientation.w = -0.156;
+    test_class_pose.position.x = 0.3;
+    test_class_pose.position.y = -0.224;
+    test_class_pose.position.z = 0.501;
+    panda_arm.MoveToPoseTarget(test_class_pose);
 
     ROS_INFO("==========Send arm to home config===========");
     ros::Duration(2.0).sleep();
