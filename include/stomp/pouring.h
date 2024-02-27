b23176272319a80c31fadeec8b97f28e73b18e88
WANG XIAOLI <71541515+lily983@users.noreply.github.com>
2024-02-27 21:00:52 +0800

    minor changes on derived classes

--- include/stomp/pouring.h
+++ include/stomp/pouring.h
@@ -8,5 +8,5 @@ class Pouring : public ExecuteStompTraj {
   protected:
     bool CallbackExecuteStompTraj(
         panda_moveit_control::ExecuteStompTraj::Request& req,
-        panda_moveit_control::ExecuteStompTraj::Response& res);
+        panda_moveit_control::ExecuteStompTraj::Response& res) override;
 };
---
+++ src/stomp/transporting.cpp
@@ -0,0 +1,56 @@
+#include "include/stomp/transporting.h"
+
+Transporting::Transporting(ros::NodeHandle& n) : ExecuteStompTraj(n) {
+    ROS_INFO("[Transporting]: finished initialization!===");
+}
+
+Transporting::~Transporting() {}
+
+bool Transporting::CallbackExecuteStompTraj(
+    panda_moveit_control::ExecuteStompTraj::Request& req,
+    panda_moveit_control::ExecuteStompTraj::Response& res) {
+    ROS_INFO(
+        "===Received request to execute STOMP trajectory for the transporting "
+        "task!===");
+    if (all_stomp_trajectory_.trajectory.size() == 0) {
+        ROS_ERROR("Haven't received STOMP trajectroy ");
+        res.success = false;
+        return 0;
+    }
+    stomp_trajectory_ = all_stomp_trajectory_.trajectory.at(req.num);
+    trajectory_msgs::JointTrajectoryPoint stomp_first_point;
+    stomp_first_point = stomp_trajectory_.joint_trajectory.points.at(0);
+
+    ROS_INFO("===Add collision scene and object to the planning scene===");
+    planning_scene_.AddCollisionMesh("mesh_scene");
+    planning_scene_.AddCollisionMesh("mesh_obj");
+
+    ROS_INFO("===Move to the first joint configuration!!!===");
+    ros::Duration(1.0).sleep();
+    if (panda_arm_.MoveToJointTarget(stomp_first_point.positions) == false) {
+        ROS_ERROR("===Failed to get to the first joint configuration!!!===");
+        res.success = false;
+        return false;
+    }
+
+    ROS_INFO(
+        "===Reached to the start joint configuration. Please handover the "
+        "test object!===");
+    ros::Duration(5.0).sleep();
+    if (GripperMoveAction(0.008, 0.05) == false) {
+        ROS_WARN("===Franka gripper failed to execute move command!===");
+    }
+    ROS_INFO("===Ready to execute STOMP trajectory! Please leave the arm!===");
+    ros::Duration(1.0).sleep();
+
+    if (panda_arm_.ExecuteTrajectory(stomp_trajectory_) == false) {
+        ROS_ERROR("===Failed to execute STOMP trajectory!===");
+        res.success = false;
+        return false;
+    }
+
+    ROS_INFO(
+        "===Finished executing STOMP trajectory for the transporting task!===");
+    res.success = true;
+    return true;
+}
